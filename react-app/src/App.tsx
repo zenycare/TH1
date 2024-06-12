496159bd36d961c8f0492028dbd4b6dcec2d10a0
Zang <ngthcgiangvvk@gmail.com>
2024-06-12 17:58:21 +0700

    add react

---
+++ react-app/src/App.tsx
@@ -0,0 +1,8 @@
+import Messsage from './messeage';
+
+
+
+function App(){
+  return <div> <Messsage/> </div>;
+}
+export default App;
---
+++ react-app/src/messeage.tsx
@@ -0,0 +1,11 @@
+
+
+function Messsage(){
+    //jsx: javascript 
+    const name="giang"
+    if(name)
+    return <h1> hello {name}</h1>;
+    return <h1>hello world</h1>
+
+}
+export default Messsage;
